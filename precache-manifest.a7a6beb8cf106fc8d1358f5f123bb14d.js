e06aa5c8cfe7a5d432e67c512ede10e9d5d57403
mxkay <kayalyxvail@gmail.com>
2020-09-29 14:27:13 -0400

    Updates

---
+++ precache-manifest.a7a6beb8cf106fc8d1358f5f123bb14d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce28f841dc9cf362e328b7b1d9ad4e38",
+    "url": "/voting_challenge_client/index.html"
+  },
+  {
+    "revision": "37e360e6d16de5d8aba3",
+    "url": "/voting_challenge_client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8732581ce00709afd1b8",
+    "url": "/voting_challenge_client/static/js/2.7f2c4a39.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/voting_challenge_client/static/js/2.7f2c4a39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e360e6d16de5d8aba3",
+    "url": "/voting_challenge_client/static/js/main.bba89577.chunk.js"
+  },
+  {
+    "revision": "8256eb880810aba1e7fa",
+    "url": "/voting_challenge_client/static/js/runtime-main.11c6fd06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/voting_challenge_client/static/media/logo.5d5d9eef.svg"
+  }
+]);
