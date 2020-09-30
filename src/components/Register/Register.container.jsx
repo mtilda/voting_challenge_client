60897a67f690ce3b8c419c232a10ee348f4126d6
mxkay <kayalyxvail@gmail.com>
2020-09-30 15:46:42 -0400

    Added comments

--- src/components/Register/Register.container.jsx
+++ src/components/Register/Register.container.jsx
@@ -4,7 +4,7 @@ import Register from "./Register";
 
 export const RegisterContainer = () => {
     const [ registrationLinks, setRegistrationLinks ] = useState([]);
-    const [ isActive, setIsActive ] = useState(false);  // when this is true, display a spinner
+    const [ isActive, setIsActive ] = useState(false);  // when this is true, display a loading status indicator
 
     useEffect( () => {
         const sheetAsJson = "https://spreadsheets.google.com/feeds/list/1ZQFCw3nzszsidf0kWWXJovEXM-5ChWtkgp2idXrZdjI/1/public/full?alt=json";
