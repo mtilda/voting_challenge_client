98a3dc3d05b0f7c1feace988ef8e4d08173072e8
Hannah Bannan <hannahbannan@MacBook-Pro-2.local>
2020-09-30 13:56:04 -0700

    spreading awareness section built and reusable button built

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
 import {NavContainer as Nav} from "./components/Nav/Nav.container";
 import { HashRouter, Route, Switch } from "react-router-dom";
-import "./App.css";
 import VoterStatsContainer from "./components/VoterStats/VoterStats.container";
-import RegisterContainer from "./components/Register/Register.container";
+import {RegisterContainer} from "./components/Register/Register.container";
+import {SpreadingAwarenessContainer as SpreadingAwareness} from "./components/SpreadingAwareness/SpreadingAwareness.container"
+import "./App.css";
 
 function App() {
   return (
@@ -22,6 +23,7 @@ function App() {
           <Route exact path="/">
             <div>
               <h2>Home</h2>
+              <SpreadingAwareness/>
             </div>
           </Route>
           <Route exact path="*">
---
+++ src/components/SpreadingAwareness/SpreadingAwareness.container.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import SpreadingAwareness from "./SpreadingAwareness";
+
+export const SpreadingAwarenessContainer = () => {
+    return <SpreadingAwareness />
+}
