69bcb8e9ac836e4e1df81eb353c428e4a68fefbe
jyoti <jshindems@gmail.com>
2020-10-01 10:34:24 -0400

    modified UI

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,7 +4,7 @@ import "./Map.css";
 import vote from '../../Images/vote.jpeg';
 
 export default ({ registrationLinks, mapHandler, statesFilling }) => (
-	<div className='App'>
+	<div className='App-txt'>
 		<h1 className='txt'>Yes! Your vote counts!</h1>
 		<div class='modal-body row'>
 			<div className='col-md-6 text-center'>
