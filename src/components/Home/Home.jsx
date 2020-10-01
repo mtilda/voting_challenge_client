ebd050e2749cf9d263c025ccb5e75ba4d6fc239f
jyoti <jshindems@gmail.com>
2020-09-30 20:10:14 -0400

    added icons and description to landingpage

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,29 @@
 import React from "react";
 import img from "../../Images/cover_page.jpeg";
 import "./Home.css";
-import { FaFontAwesome, FaRegEdit } from "react-icons/fa";
+import { FaRegEdit, FaBookOpen, FaMapMarked, FaRegAddressCard, FaVoteYea } from "react-icons/fa";
 
 export default () => (
     <>
         <div className="bg"></div>
-        <div className = "text-center">
-        <FaFontAwesome icon = {FaFontAwesome} className = "icon"/>
+        <h1 className="title">
+            How to Vote
+        </h1>
+        <div className = "text-center" >
+            <FaRegEdit size={59} color="#DA313C"/>
+            <span className="text-center"> Register to vote with your state</span>
+        </div>
+        <div className="text-center">
+            <FaMapMarked size={59} color= "#DA313C"/>
+            <span className="text-center"> Find your polling location or get a mail-in ballot</span>
+        </div>
+        <div className="text-center">
+            <FaRegAddressCard size={59} color="#DA313C"/>
+            <span className="text-center"> Check if you need an ID</span>
+        </div>
+        <div className="text-center">
+            <FaVoteYea size={59} color="#DA313C"/>
+            <span className="text-center"> Vote!!</span>
         </div>
     </>
 )
