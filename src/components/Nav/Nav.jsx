69f7c450a8efbf5634fb8c49b07b49a630ceed57
jyoti <jshindems@gmail.com>
2020-10-01 15:26:45 -0400

    styled nav links

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,17 +1,17 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import "./Nav.css";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Nav.css';
 
 export const Nav = () => (
-  <div className="nav">
-    <Link to="/voting-stats" className="nav-link">
-      Voting Statistics
-    </Link>
-    <Link to="/register" className="nav-link">
-      Register Now
-    </Link>
-    <Link to="/about" className="nav-link">
-      About Us
-    </Link>
-  </div>
+	<div className="nav">
+	  <Link to="/voting-stats" className="nav">
+	    Voting Statistics
+	  </Link>
+	  <Link to="/register" className="nav">
+	    Register Now
+	  </Link>
+	  <Link to="/about" className="nav">
+	    About Us
+	  </Link>
+	</div>
 );
