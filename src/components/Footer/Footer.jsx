2ac280bc110ba1a68c990708c111ace25e91fd82
Kay <63630918+mxkay@users.noreply.github.com>
2020-09-30 20:12:23 -0400

    Removed unnecessary parenthesis

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,8 @@
 import React from "react";
 import "./Footer.css"
 
-export default () => (
+export default () =>
   <div className="footer">
     <p>© Runtime Terrors 2020</p>
     <p>Privacy Policy</p>
-  </div>
-);
+  </div>;
