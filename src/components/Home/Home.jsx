e9960f51c57661a895a2c7f6184dc5efa3402f22
Hannah Bannan <hannahbannan@MacBook-Pro-2.local>
2020-10-01 10:57:41 -0700

    landing page text styled

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import img from "../../Images/cover_page.jpeg";
+import {Container, Row, Col} from "react-bootstrap";
 import { Link } from 'react-router-dom';
 import {SpreadingAwareness} from "../SpreadingAwareness/SpreadingAwareness";
 import {VoterTurnout} from "../VoterTurnout/VoterTurnout";
@@ -9,12 +10,19 @@ import "./Home.css";
 import { FaRegEdit, FaBookOpen, FaMapMarked, FaRegAddressCard, FaVoteYea } from "react-icons/fa";
 
 export const Home = () => (
+<Container fluid style={{paddingLeft: 0, paddingRight: 0, paddingTop: 0, paddingBottom: 0}}>
 	<div className='home'>
 		<div className='bg'>
+            <Row>
+                <Col sm="6"></Col>
+                <Col>
 			<h2 className='floating-question'>
-				Have <span className='red'>YOU</span> registered to{' '}
-				<span className='red'>VOTE</span>?
+				Have <span className='red'> YOU </span> registered to 
+				<span className='red'>VOTE</span> ?
 			</h2>
+            </Col>
+            <Col sm="1"></Col>
+            </Row>
 			<div class='text-block'>
 				<h4>Let Your Voice be heard!</h4>
 				<Link to = "/register">
@@ -45,4 +53,5 @@ export const Home = () => (
 		<SpreadingAwareness />
 		<VoterTurnout />
 	</div>
+    </Container>
 );
