29950aebe2fc717a734d3bb63b5d8af5daf5fec5
Hannah Bannan <hannahbannan@MacBook-Pro-2.local>
2020-10-01 13:35:55 -0700

    responsive nav bar styled

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,33 +5,21 @@ import logo from "../../Images/logo.png";
 import "./Nav.css";
 
 export const Nav = () => (
-  <Container>
-    <Row>
+
 	<div className="nav">
-    <Col xs="1"></Col>
-    <Col>
     <Link to = "/" >
       <img src={logo} class="l" />
     </Link>
-    </Col>
-    <Col></Col>
-    <Col>
+      <div className="nav-links">
 	  <Link to="/voting-stats" className="nav">
 	    Voting Statistics
 	  </Link>
-    </Col>
-    <Col>
 	  <Link to="/register" className="nav">
 	    Register Now
 	  </Link>
-    </Col>
-    <Col>
 	  <Link to="/about" className="nav">
 	    About Us
 	  </Link>
-    </Col>
-    <Col xs="1"></Col>
+    </div>
 	</div>
-  </Row>
-  </Container>
 );
