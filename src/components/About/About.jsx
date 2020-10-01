15f4148b808b6c8f5201da0ce37ebaed5a4e039f
Hannah Bannan <hannahbannan@MacBook-Pro-2.local>
2020-10-01 08:06:38 -0700

    team section built

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -24,46 +24,54 @@ export const About = () => {
         <h2>Meet Our Team</h2>
         <div className="devs">
           <div className="dev-ind">
-            <img src="" alt="" />
-            <a href=""></a>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564108/Voting%20App/20200821_160142_1_sh09ix.png" alt="Kay Vail" />
+       <br/>
+       <a href="https://www.linkedin.com/in/kayvail/" target="_blank">Kay Vail</a>
             <p>Full Stack Developer</p>
           </div>
           <div className="dev-ind">
-            <img src="" alt="" />
-            <a href=""></a>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564213/Voting%20App/IMG-8119-Original_1_xxjlwp.png" alt="Jyoti Shinde" />
+            <br/>
+            <a href="https://www.linkedin.com/in/jyoti-shinde/" target="_blank">Jyoti Shinde</a>
             <p>Full Stack Developer</p>
           </div>
           <div className="dev-ind">
-            <img src="" alt="" />
-            <a href=""></a>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564281/Voting%20App/Sean_1_vcwitf.png" alt="Sean O'Brien" />
+            <br/>
+            <a href="https://www.linkedin.com/in/sean-t-obrien/" target="_blank">Sean O'Brien</a>
             <p>Full Stack Developer</p>
           </div>
           <div className="dev-ind">
-            <img src="" alt="" />
-            <a href=""></a>
-            <p>Full Stack Developer</p>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564366/Voting%20App/HB_Headshot_1_yyakls.png" alt="Hannah Bannan" />
+            <br/>
+            <a href="https://www.linkedin.com/in/hannahbannan/" target="_blank">Hannah Bannan</a>
+            <p>Front End Developer</p>
           </div>
         </div>
         <div className="designers">
           <div className="des-ind">
-            <img src="" alt="" />
-            <a href=""></a>
-            <p>Full Stack Developer</p>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564417/Voting%20App/KCW_1_xbnhuo.png" alt="Kristin White" />
+            <br/>
+            <a href="https://www.linkedin.com/in/kristin-clark-white/" target="_blank">Kristin White</a>
+            <p>UX Designer</p>
           </div>
           <div className="des-ind">
-            <img src="" alt="" />
-            <a href=""></a>
-            <p>Full Stack Developer</p>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564483/Voting%20App/IMG_20200930_162042_1_rukafk.png" alt="Christian Pirnie" />
+            <br/>
+            <a href="https://www.linkedin.com/in/christianpirnie/" target="_blank">Christian Pirnie</a>
+            <p>UX Researcher</p>
           </div>
           <div className="des-ind">
-            <img src="" alt="" />
-            <a href=""></a>
-            <p>Full Stack Developer</p>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564538/Voting%20App/20_Facebook_2_mdbzef.png" alt="Prezetta McCall" />
+            <br/>
+            <a href="https://www.linkedin.com/in/prezetta-mccall/" target="_blank">Prezetta McCall</a>
+            <p>UX/UI Designer</p>
           </div>
           <div className="des-ind">
-            <img src="" alt="" />
-            <a href=""></a>
-            <p>Full Stack Developer</p>
+            <img src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564608/Voting%20App/WhatsApp_Image_2020-09-30_at_5.21_1_ktgdwj.png" alt="Archana Mirkhelkar" />
+            <br/>
+            <a href="https://www.linkedin.com/in/archanamirkhelkar/" target="_blank">Archana Mirkhelkar</a>
+            <p>UX Designer</p>
           </div>
         </div>
       </div>
