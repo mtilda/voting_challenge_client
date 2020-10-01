2825d17d70f96788fe7c8d4b8c9debb40249d057
Hannah Bannan <hannahbannan@MacBook-Pro-2.local>
2020-10-01 10:23:33 -0700

    team section styled and respnsive

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -49,123 +49,149 @@ export const About = () => {
         </Row>
         <Row>
           <Col></Col>
-          <Col>
+          <Col xs="10">
             <div className="team-row">
-              <div className="team-ind">
-                <img
-                  src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564108/Voting%20App/20200821_160142_1_sh09ix.png"
-                  alt="Kay Vail"
-                />
-                <br />
-                <a href="https://www.linkedin.com/in/kayvail/" target="_blank">
-                  Kay Vail
-                </a>
-                <p>Full Stack Developer</p>
-              </div>
-              <div className="team-ind">
-                <img
-                  src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564213/Voting%20App/IMG-8119-Original_1_xxjlwp.png"
-                  alt="Jyoti Shinde"
-                />
-                <br />
-                <a
-                  href="https://www.linkedin.com/in/jyoti-shinde/"
-                  target="_blank"
-                >
-                  Jyoti Shinde
-                </a>
-                <p>Full Stack Developer</p>
-              </div>
-              <div className="team-ind">
-                <img
-                  src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564281/Voting%20App/Sean_1_vcwitf.png"
-                  alt="Sean O'Brien"
-                />
-                <br />
-                <a
-                  href="https://www.linkedin.com/in/sean-t-obrien/"
-                  target="_blank"
-                >
-                  Sean O'Brien
-                </a>
-                <p>Full Stack Developer</p>
-              </div>
-              <div className="team-ind">
-                <img
-                  src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564366/Voting%20App/HB_Headshot_1_yyakls.png"
-                  alt="Hannah Bannan"
-                />
-                <br />
-                <a
-                  href="https://www.linkedin.com/in/hannahbannan/"
-                  target="_blank"
-                >
-                  Hannah Bannan
-                </a>
-                <p>Front End Developer</p>
-              </div>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564108/Voting%20App/20200821_160142_1_sh09ix.png"
+                    alt="Kay Vail"
+                  />
+
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/kayvail/"
+                    target="_blank"
+                  >
+                    Kay Vail
+                  </a>
+                  <p>Full Stack Developer</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564213/Voting%20App/IMG-8119-Original_1_xxjlwp.png"
+                    alt="Jyoti Shinde"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/jyoti-shinde/"
+                    target="_blank"
+                  >
+                    Jyoti Shinde
+                  </a>
+                  <p>Full Stack Developer</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564281/Voting%20App/Sean_1_vcwitf.png"
+                    alt="Sean O'Brien"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/sean-t-obrien/"
+                    target="_blank"
+                  >
+                    Sean O'Brien
+                  </a>
+                  <p>Full Stack Developer</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564366/Voting%20App/HB_Headshot_1_yyakls.png"
+                    alt="Hannah Bannan"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/hannahbannan/"
+                    target="_blank"
+                  >
+                    Hannah Bannan
+                  </a>
+                  <p>Front End Developer</p>
+                </div>
+              </Col>
             </div>
           </Col>
           <Col></Col>
         </Row>
-        <div className="team-row">
-          <div className="team-ind">
-            <img
-              src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564417/Voting%20App/KCW_1_xbnhuo.png"
-              alt="Kristin White"
-            />
-            <br />
-            <a
-              href="https://www.linkedin.com/in/kristin-clark-white/"
-              target="_blank"
-            >
-              Kristin White
-            </a>
-            <p>UX Designer</p>
-          </div>
-          <div className="team-ind">
-            <img
-              src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564483/Voting%20App/IMG_20200930_162042_1_rukafk.png"
-              alt="Christian Pirnie"
-            />
-            <br />
-            <a
-              href="https://www.linkedin.com/in/christianpirnie/"
-              target="_blank"
-            >
-              Christian Pirnie
-            </a>
-            <p>UX Researcher</p>
-          </div>
-          <div className="team-ind">
-            <img
-              src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564538/Voting%20App/20_Facebook_2_mdbzef.png"
-              alt="Prezetta McCall"
-            />
-            <br />
-            <a
-              href="https://www.linkedin.com/in/prezetta-mccall/"
-              target="_blank"
-            >
-              Prezetta McCall
-            </a>
-            <p>UX/UI Designer</p>
-          </div>
-          <div className="team-ind">
-            <img
-              src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564608/Voting%20App/WhatsApp_Image_2020-09-30_at_5.21_1_ktgdwj.png"
-              alt="Archana Mirkhelkar"
-            />
-            <br />
-            <a
-              href="https://www.linkedin.com/in/archanamirkhelkar/"
-              target="_blank"
-            >
-              Archana Mirkhelkar
-            </a>
-            <p>UX Designer</p>
-          </div>
-        </div>
+        <Row>
+          <Col></Col>
+          <Col xs="10">
+            <div className="team-row">
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564417/Voting%20App/KCW_1_xbnhuo.png"
+                    alt="Kristin White"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/kristin-clark-white/"
+                    target="_blank"
+                  >
+                    Kristin White
+                  </a>
+                  <p>UX Designer</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564483/Voting%20App/IMG_20200930_162042_1_rukafk.png"
+                    alt="Christian Pirnie"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/christianpirnie/"
+                    target="_blank"
+                  >
+                    Christian Pirnie
+                  </a>
+                  <p>UX Researcher</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564538/Voting%20App/20_Facebook_2_mdbzef.png"
+                    alt="Prezetta McCall"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/prezetta-mccall/"
+                    target="_blank"
+                  >
+                    Prezetta McCall
+                  </a>
+                  <p>UX/UI Designer</p>
+                </div>
+              </Col>
+              <Col>
+                <div className="team-ind">
+                  <img
+                    src="https://res.cloudinary.com/hannahbannan/image/upload/v1601564608/Voting%20App/WhatsApp_Image_2020-09-30_at_5.21_1_ktgdwj.png"
+                    alt="Archana Mirkhelkar"
+                  />
+                  <br />
+                  <a
+                    href="https://www.linkedin.com/in/archanamirkhelkar/"
+                    target="_blank"
+                  >
+                    Archana Mirkhelkar
+                  </a>
+                  <p>UX Designer</p>
+                </div>
+              </Col>
+            </div>
+          </Col>
+          <Col></Col>
+        </Row>
       </div>
     </Container>
   );
